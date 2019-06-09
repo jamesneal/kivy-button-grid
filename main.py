a5e342c568eafb11c7eccad6ae4846dd3b192a6f
Laen <laen@oshpark.com>
2019-06-09 09:57:33 -0700

    Now with a loop.

--- main.py
+++ main.py
@@ -13,32 +13,25 @@ from kivy.uix.image import Image
 os.environ['KIVY_GL_BACKEND'] = 'gl'
 
 def button(num, event):
-    print("Button {} is pressed.".format(num))
+    if (num == 1):
+      print("I love it when button 1 is pressed.")
+    elif (num == 4):
+      print("Come on.  Button 4 is dumb.")
+    else:
+      print("Button {} is pressed.".format(num))
 
 class ImageButton(ButtonBehavior, Image):
     pass
 
 class TestApp(App):
     def build(self):
-        #layout = GridLayout(cols=2, row_force_default=True, col_default_width=100)
         layout = StackLayout(orientation='lr-tb')
 
-        btn1 = ImageButton(source="images/1.png", size_hint_x=0.5, size_hint_y=0.5)
-        btn1.bind(on_press=partial(button,1))
+        for i in range(1,5):
+          btn = ImageButton(source="images/{}.png".format(i), size_hint_x=0.5, size_hint_y=0.5)
+          btn.bind(on_press=partial(button,i))
+          layout.add_widget(btn)
 
-        btn2 = ImageButton(source="images/2.png", size_hint_x=0.5, size_hint_y=0.5)
-        btn2.bind(on_press=partial(button,2))
-
-        btn3 = ImageButton(source="images/3.png", size_hint_x=0.5, size_hint_y=0.5)
-        btn3.bind(on_press=partial(button,3))
-
-        btn4 = ImageButton(source="images/4.png", size_hint_x=0.5, size_hint_y=0.5)
-        btn4.bind(on_press=partial(button,4))
-
-        layout.add_widget(btn1)
-        layout.add_widget(btn2)
-        layout.add_widget(btn3)
-        layout.add_widget(btn4)
         return layout
 
 TestApp().run()
